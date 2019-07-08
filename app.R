cdbbfbef381e3e293df952768b89292a65a5cea9
HarukoNakao <42297576+HarukoNakao@users.noreply.github.com>
2019-07-08 16:57:13 +0100

    - Change the structure of interfece - Enable to input station/bonus/destination - Now scorebord has two column for one team (group a and b)

--- app.R
+++ app.R
@@ -7,26 +7,82 @@ library(leaflet)
 
 # minimum data needed for game --------------------------------------------
 
-# todo: add 2 groups per team
+#added two groups to each team
 team_names = 1:3
-team_columns_names = paste0("team_", team_names)
+groups = 1:2
+rep_teams = replicate(length(groups),team_names)
+rep_teams = sort(rep_teams)
+team_columns_names = paste0("team_",rep_teams,"_group_",groups)
+
+#Group list
+group_list <- c("Group A","Group B")
+
+#Station names list
+StationName =read.csv("Metro_station_score.csv")
+StationName$Points <- NULL
+
+DestName = read.csv("Dest_Score.csv")
+DestName$Score <-NULL 
+
+BonusName = read.csv("Bonus.csv")
+BonusName$Score <-NULL
 
 # Idea: select team at the beginning
 # stations = readr::read_csv("stations.csv") # todo: create stations .csv with name and number points
 # robin to geocode them and put on map
 stations = sf::read_sf("stations.geojson")
-
 initial_scores = rep(0, nrow(stations))
 
 # test scoring: group 2 has scored in station 1, 3 has scored in 1 and 2
-team_initial_scores = replicate(length(team_names), initial_scores)
+team_initial_scores = replicate(length(team_columns_names), initial_scores)
 team_initial_scores[1, 2] = 1
 team_initial_scores[1:2, 3] = 1
 scoresheet = cbind(sf::st_drop_geometry(stations), team_initial_scores)
 names(scoresheet)[4:ncol(scoresheet)] = team_columns_names
 
-ui <- navbarPage(
-  title = "Metropoly game",
+ui <- fluidPage(
+  headerPanel(tags$img(src = "Metropoly_Logo.png")),
+  sidebarLayout(
+    sidebarPanel( 
+      h3("Select your team"),
+    selectInput(
+    inputId = "teamname",
+    label = "Team name",
+    choices = team_names,
+    selected = team_names[1]
+  ),
+ 
+  radioButtons(
+    inputId = "Group",
+    label = "Your group",
+    choices = list("Group A" = 1,
+                   "Group B" = 2),
+    selected = 1
+  ),
+  h3("Log your score"),
+  selectInput(
+    inputId = "VisitStation",
+    label = "Select the STATION you visited",
+    choices = StationName,
+    selected = StationName[1]
+  ),
+  selectInput(
+    inputId ="VisitDest",
+    label = "Select the DESTINATION you visited",
+    choice = DestName,
+    selected = DestName[1]
+  ),
+  selectInput(
+    inputId = "ScoredBonous",
+    label = "Select a Bonous type",
+    choices = Bonus_list,
+    selected = Bonus_list[1]
+  ),
+
+  submitButton(text = "Submit")),
+  
+  mainPanel(
+  tabsetPanel(
   tabPanel(
     title = "Game rules",
     h1("How to play Metropoly game "),
@@ -80,30 +136,17 @@ ui <- navbarPage(
       "Annoy and intimidate your rival groups by tweeting pictures of each point scoring destination you arrive at to the hashtag #ITSmetropoly.
       We’ll be looking at the twitter feed during the day and highlighting/promoting any particularly funny/good comments and photos.
       "
-    )
     ),
-  tabPanel(title = "Tips to win",
-           h1("Advices"),
-           tags$li("Plan your route in advance. Although finding out about trains whilst on the move is relatively easy, it is much harder to determine which bus to take and even whether a bus service to a specific destination is available."),
-
-                      h1("Resources")),
-  tabPanel(
-    h1("Teams: log your scores here!"),
-    title = "Team scoring tab",
-    selectInput(
-      inputId = "teamname",
-      label = "Select your team name to see your score",
-      choices = team_names,
-      selected = team_names[1]
-    ),
+    h1("Advices"),
+    tags$li("Plan your route in advance. Although finding out about trains whilst on the move is relatively easy, it is much harder to determine which bus to take and even whether a bus service to a specific destination is available."),
     
-    radioButtons(
-      inputId = "Group",
-      label = "Your group",
-      choices = list("Group A" = 1,
-                     "Group B" = 2),
-      selected = 1
+    h1("Resources")
     ),
+  tabPanel(
+    h1("Your current score"),
+    title = "Team scoring tab",
+    shiny::textOutput("shiny_teamname"),
+    shiny::textOutput("shiny_group"),
     shiny::tableOutput("shiny_scoresheet")
   ),
   tabPanel(title = "Leader board"),
@@ -114,12 +157,14 @@ ui <- navbarPage(
     actionButton("recalc", "New points")
   )
   
-    )
+    ))))
 
 server <- function(input, output) {
   # If we get the scores from Google Sheets:
   # scoresheet = function_to_read_from_google_sheets()
-  output$shiny_scoresheet = renderTable(scoresheet[c(names(scoresheet)[1:2], paste0("team_", input$teamname))])
+  output$shiny_teamname = renderText(paste("Team Name:",input$teamname))
+  output$shiny_group = renderText(group_list[as.numeric(input$Group)])
+  output$shiny_scoresheet = renderTable(scoresheet[c(names(scoresheet)[1:2], paste0("team_",input$teamname,"_group_",groups))])
   
   # This is the map
   points <- eventReactive(input$recalc, {
